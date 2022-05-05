7948f54da38df8b3105fe020861a53bd5ee5979b
Claudir Santos <102737368+TerrorK1ng@users.noreply.github.com>
2022-05-05 09:01:17 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+let X = 3;
+let Y = 5;
+
+const Numero1 = 50;
+const Numero2 = 90;
+const Numero3 = 35;
+const Salario = 1800;
+
+/*  Exercicios De Cálculos 1*/
+
+// A
+console.log(10+20*30);
+
+// B
+console.log(4**2/30);
+
+// C
+console.log((9**4+2)*6-1);
+
+/*  Exercício da 2*/
+console.log(10%3*10**1+1-10*4/2);
+
+// Exercício da 3
+console.log('Dev Claudir Santos');
+
+// Exercicios da 4
+console.log(2*X*3*Y);
+
+// Exercicios da 5
+console.log(Numero1+Numero2+Numero3);
+
+// Exercicios da 6
+console.log(Salario*2);
